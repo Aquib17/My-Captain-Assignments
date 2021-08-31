b35d7ad885c330348487ebf15b95c4ea80a3d58c
Aquib17 <89248671+Aquib17@users.noreply.github.com>
2021-08-31 17:17:13 +0530

    Update Web Scraping.py

--- Web Scraping.py
+++ Web Scraping.py
@@ -25,25 +25,28 @@ for page_num in range(1, page_num_MAX):
     for hotel in all_hotels:                                                
       	hotel_dict = {}                                                         
       	hotel_dict["name"] = hotel.find("h3", {"class": "listingHotelDescription__hotelName"}).text  
-	      hotel_dict["address"] = hotel.find("span", {"itemprop": "streetAddress"}).text   
-	      hotel_dict["price"] = hotel.find("span", {"class": "listingPrice__finalPrice"}).text  
+        hotel_dict["address"] = hotel.find("span", {"itemprop": "streetAddress"}).text   
+        hotel_dict["price"] = hotel.find("span", {"class": "listingPrice__finalPrice"}).text  
 
       	# try .... except                                                                  
-	      try:                                                                                              
-		        hotel_dict["rating"] = hotel.find("span", {"class": "hotelRating__ratingSummary"}).text                   
-		    except Attributer:                                                                                           
-			      hotel_dict["rating"] = None                                                             
-			  parent_amenities_element = hotel.find("div", "class": "amenityWrapper"})                 
+	try:                                                                                              
+            hotel_dict["rating"] = hotel.find("span", {"class": "hotelRating__ratingSummary"}).text                   
+        except Attributer:                                                                                           
+            hotel_dict["rating"] = None                                                             
+			  
+	parent_amenities_element = hotel.find("div", "class": "amenityWrapper"})                 
 			
-			  amenities_list = []                                                                               
-			  for amenity in parent_amenities_element.find("div", {"class": "amenityWrapper__amenity"}):   
-				    amenities_list.append(amenity.find("span", {"class": "d-body-sm"}).text.strip())      
-				hotel_dict["amenities"] = ', '.join(amenities_list[:-1])                             
+	amenities_list = []                                                                               
+        for amenity in parent_amenities_element.find("div", {"class": "amenityWrapper__amenity"}):   
+	    amenities_list.append(amenity.find("span", {"class": "d-body-sm"}).text.strip())      
 				
-				scrapped_info_list.append(hotel_dict)   
-				connect.insert_into_table(args.dbname, tuple(hotel_dict,values()))    
+	hotel_dict["amenities"] = ', '.join(amenities_list[:-1])                             
 				
-				# print(hotel_name, hotel_address, hotel_price, hotel_rating, amenities_list)  
+	scrapped_info_list.append(hotel_dict)   
+	connect.insert_into_table(args.dbname, tuple(hotel_dict,values()))    
+				
+	# print(hotel_name, hotel_address, hotel_price, hotel_rating, amenities_list)  
+
 dataFrame = pandas.DataFrame(scraped_info_list)                                
 print("Creating csv file...")                                  
 dataFrame.to_csv("Oyo.csv")           				
